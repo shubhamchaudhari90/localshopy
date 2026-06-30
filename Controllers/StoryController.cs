40cab76384f6f1eb7906cb7fff7bf4f705de752f
Shubham <shubhamchaudhari90@gmail.com>
2026-06-30 22:51:00 +0530

    Added Story section

---
+++ Controllers/StoryController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace localshopyNew.Controllers
+{
+    public class StoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Topiwala()
+        {
+            return View();
+        }
+
+        public IActionResult SasaKasav()
+        {
+            return View();
+        }
+
+        public IActionResult Lakudtodya()
+        {
+            return View();
+        }
+
+        public IActionResult HusharMasa()
+        {
+            return View();
+        }
+    }
+}
